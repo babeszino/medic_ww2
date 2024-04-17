216fe6986ea63432ed1d520baff10e37600c7d85
Sámson Bálint <balintsamson@gmail.com>
2024-04-17 23:52:53 +0200

    WASD movement is back, also added sprinting

--- medic.gd
+++ medic.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
 
-var speed = 300.0
-var target_position = position
+# egyelore clamp-eltem, hogy csak a viewport szeleig lehessen menni, 
+# de majd kiszedhetjuk
 
+var speed = 300.0
+var sprint_speed = 450.0
+var screen_size
 var movement = false
 
 var blood = 100
@@ -16,28 +19,34 @@ var left_leg = {'flesh': 100, 'bone': 0, 'wound': false, 'attached': true}
 var right_leg = {'flesh': 100, 'bone': 0, 'wound': false, 'attached': false}
 
 
-func _physics_process(delta):
-	var direction = target_position - position
-	
-	if direction.length() > speed * delta:
-		direction = direction.normalized() * speed
-	else:
-		direction = target_position - position
+func _ready():
+	screen_size = get_viewport_rect().size
 
-	position += direction * delta
-	move_and_slide()
+
+func _process(delta):
+	var velocity = Vector2.ZERO
+	var current_speed = speed
+	
+	if Input.is_action_pressed("go_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("go_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("go_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("go_right"):
+		velocity.x += 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized()
+		
+		if Input.is_action_pressed("sprint"):
+			current_speed = sprint_speed
+		
+		velocity *= current_speed
+	
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
 
 func start(pos):
 	position = pos
-	target_position = pos
 	#$CollisionShape2D.disabled = false
-
-func _unhandled_input(event):
-	if event is InputEventMouseButton and event.is_action("go_to"):
-		target_position = event.position
-		movement = !movement
-		print(movement)
-	
-	#Movement change: 
-	if event is InputEventMouseMotion and movement:
-		target_position = event.position
